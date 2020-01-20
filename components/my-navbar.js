bdb4b0a9feb44305eb01821e961e43f431ee286e
FroVolod <frol_off@meta.ua>
2020-01-20 12:37:30 +0200

    chanhed css

--- components/my-navbar.js
+++ components/my-navbar.js
@@ -23,12 +23,12 @@ export default class extends React.Component {
               Продам дом с большим участком в ближайшем пригороде по цене квартиры в Харькове! 
             </h1>
           </Col>
-          <Col md="12" lg="4">
+          <Col className="phone" md="12" lg="4">
             <a href="tel:+380675708021"><img src={"/static/icons/phone.png"}/></a>
             {/* <a href="tel:+380675708026"><Button color="warning" size="sm" outline><img src={"/static/images/icons/phone.png"}/></Button></a> */}
             Владимир +38 (067) 570-80-21
           </Col>
-          <Col md="12" lg="4">
+          <Col className="phone" md="12" lg="4">
             <a href="tel:+380933207601"><img src={"/static/icons/phone.png"}/></a>
             Татьяна &emsp;+38 (093) 320-76-01
           </Col>
@@ -38,12 +38,21 @@ export default class extends React.Component {
             h2 {
                 color: #ed143dad;
             }
+            img {
+              margin-right: 20px;
+            }
+            .phone {
+              padding-right: 15px;
+              padding-left: 15px;
+              padding-top: 15px;
+            }
             .nav-item p {
                 color: rgba(0,0,0,.9);
             }
             .navbar {
                 background: #22b5ff;
                 margin: 0;
+                padding-top: 20px;
             }
           `}
         </style>
